bf99f60a0215b968a051c87b80a46a0b12028cad
Adriano Henrique <adriano.henrique.eng@gmail.com>
2022-03-31 17:07:58 -0300

    Adicionado criação de número aleatório

--- projetos/guess my number/guess_number.rb
+++ projetos/guess my number/guess_number.rb
@@ -8,3 +8,8 @@ print "What's your name?"
 input = gets
 name = input.chomp
 puts "Welcome, #{name}!"
+
+# Armazena um número aleatório entre 1 e 100
+puts "I've got a random number between 1 and 100."
+puts 'Can you guess it?'
+target = rand(1..100) # equivale a rand(100)+1
