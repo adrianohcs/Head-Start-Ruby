5fa7d18ac1c1d9612ca88ddb05aacc9db5022aa8
Adriano Henrique <adriano.henrique.eng@gmail.com>
2022-03-31 17:19:18 -0300

    Adicionada condição caso jogador perca o jogo

--- projetos/guess my number/guess_number.rb
+++ projetos/guess my number/guess_number.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 puts 'Welcome to Guess My Number!'
-print "What's your name?"
+print "What's your name? "
 
 # Nome através do input do usuário
 # 'Chomp' p/ retirar a newline após a entrada
@@ -13,3 +13,13 @@ puts "Welcome, #{name}!"
 puts "I've got a random number between 1 and 100."
 puts 'Can you guess it?'
 target = rand(1..100) # equivale a rand(100)+1
+
+# Armazena quantas tentativas o jogador gastou
+num_guesses = 0
+
+# Indicador se o jogador acertou ou não
+guessed_it = false
+
+# Caso o jogador perca o jogo, retorna qual era o número
+puts "Sorry, #{name}... You ran out of tries. BTW my number was #{target}." unless guessed_it
+# Mesmo que: unless guessed_it; puts "Sorry, #{name}... You ran out of tries. BTW my number was #{target}."; end
